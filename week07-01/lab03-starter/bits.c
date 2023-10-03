109640ecb75f0201382d2dbcd863d27f59813854
Greg Benson <benson@cs.usfca.edu>
2023-10-03 10:20:47 -0700

    Added week07-01.

---
+++ week07-01/lab03-starter/bits.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include "bits.h"
+
+uint32_t get_bits(uint64_t num, uint32_t start, uint32_t count) {
+    uint64_t mask = (1 << count) - 1;
+    return (num >> start) & mask;
+}
+
+int64_t sign_extend(uint64_t num, uint32_t start) {
+    uint32_t dist = 64 - start;
+    uint64_t shifted_bits = num << dist;
+    int64_t signed_bits = ((int64_t) shifted_bits) >> dist;
+    return signed_bits;
+}
---
+++ week07-01/lab03-starter/bits.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+
+uint32_t get_bits(uint64_t num, uint32_t start, uint32_t count);
+int64_t sign_extend(uint64_t num, uint32_t start);
